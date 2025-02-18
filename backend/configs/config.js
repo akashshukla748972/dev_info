f97bbfcfa7f8d886a75c319a90090ef6aa2a61a2
akashshukla748972 <shuklaakash748972@gmail.com>
2025-02-18 14:58:48 +0530

    add generate pashword hash

--- backend/configs/config.js
+++ backend/configs/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 const global_variable = {
   port: process.env.PORT,
   mongo_uri: process.env.MONGO_URI,
+  jwt_secret: process.env.JWT_SECRET,
 };
 
 export default global_variable;
