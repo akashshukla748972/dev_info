3322e9fb87d6cca1061edbd83110c6c3561b4949
akashshukla748972 <shuklaakash748972@gmail.com>
2025-06-04 17:15:08 +0530

    add required admin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,17 @@ import AdminView from "./pages/admin_view";
 import CheckAuth from "./components/common_view/check-auth";
 import ClientLayout from "./components/client_view/client_layout";
 import AdminLayout from "./components/admin_view/admin_layout";
+import About from "./pages/admin_view/about";
+import Skills from "./pages/admin_view/skill";
+import Projects from "./pages/admin_view/projects";
+import Experience from "./pages/admin_view/experience";
+import Education from "./pages/admin_view/education";
+import Service from "./pages/admin_view/service";
+import Testimonial from "./pages/admin_view/testimonial";
+import Blog from "./pages/admin_view/blog";
+import Message from "./pages/admin_view/message";
+import Setting from "./pages/admin_view/setting";
+import Logout from "./pages/admin_view/logout";
 
 const App = () => {
   const role = "admin";
@@ -31,17 +42,17 @@ const App = () => {
           }
         >
           <Route path="dashboard" element={<AdminView />} />
-          <Route path="about" element={<AdminView />} />
-          <Route path="skills" element={<AdminView />} />
-          <Route path="projects" element={<AdminView />} />
-          <Route path="experiences" element={<AdminView />} />
-          <Route path="educations" element={<AdminView />} />
-          <Route path="services" element={<AdminView />} />
-          <Route path="testimonials" element={<AdminView />} />
-          <Route path="blogs" element={<AdminView />} />
-          <Route path="messages" element={<AdminView />} />
-          <Route path="settings" element={<AdminView />} />
-          <Route path="logout" element={<AdminView />} />
+          <Route path="about" element={<About />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="experiences" element={<Experience />} />
+          <Route path="educations" element={<Education />} />
+          <Route path="services" element={<Service />} />
+          <Route path="testimonials" element={<Testimonial />} />
+          <Route path="blogs" element={<Blog />} />
+          <Route path="messages" element={<Message />} />
+          <Route path="settings" element={<Setting />} />
+          <Route path="logout" element={<Logout />} />
         </Route>
       </Routes>
     </div>
---
+++ client/src/pages/admin_view/testimonial.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <h1>Testimonial Page</h1>
+    </div>
+  );
+};
+
+export default Testimonial;
