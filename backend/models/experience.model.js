09305cfae8cc0e9901473bea7a96df20e792fa7a
akashshukla748972 <shuklaakash748972@gmail.com>
2025-06-09 13:59:51 +0530

    Add experience model

---
+++ backend/models/experience.model.js
@@ -0,0 +1,67 @@
+import mongoose, { model } from "mongoose";
+
+const experienceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    company: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+    },
+    isCurrent: {
+      type: Boolean,
+      default: false,
+    },
+    technologies: [
+      {
+        type: String, // ["React", "Node.js", "MongoDB"]
+        trim: true,
+      },
+    ],
+    logo: {
+      public_id: {
+        type: String,
+        default: null,
+      },
+      url: {
+        type: String,
+        default: null,
+      },
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "admin",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const experienceModel = model("experience", experienceSchema);
+
+export default experienceModel;
