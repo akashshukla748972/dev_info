66fb6c6f7eb043d20580e6cc821795a36b0b63e1
akashshukla748972 <shuklaakash748972@gmail.com>
2025-06-10 16:34:17 +0530

    Add education get-all, update and delete module

--- backend/controllers/education/education.controller.js
+++ backend/controllers/education/education.controller.js
@@ -69,3 +69,116 @@ export const handleCreateEducation = async (req, res, next) => {
     return next(new CustomError("Internal server error", 500));
   }
 };
+
+export const handleGetAllAdminEducation = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const adminEducations = await educationModel.find({ createdBy: id });
+    if (adminEducations.length == 0) {
+      return next(new CustomError("Education not found.", 404));
+    }
+
+    return res.status(200).json({
+      message: "Getting all admin educations successfully.",
+      data: adminEducations,
+      isSuccess: true,
+      isError: false,
+    });
+  } catch (error) {
+    console.error("Error while getting all admin education:", error);
+    return next(new CustomError("Internal server error", 500));
+  }
+};
+
+export const handleDeleteEducation = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { id } = req.params;
+
+    const deletedEducations = await educationModel.findOneAndDelete({
+      createdBy: userId,
+      _id: id,
+    });
+    if (!deletedEducations) {
+      return next(new CustomError("Education not found.", 404));
+    }
+
+    return res.status(200).json({
+      message: "Education delete successfully.",
+      data: deletedEducations,
+      isSuccess: true,
+      isError: false,
+    });
+  } catch (error) {
+    console.error("Error while deleting education:", error);
+    return next(new CustomError("Internal server error", 500));
+  }
+};
+
+export const handleUpdateEducation = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { id } = req.params;
+
+    const {
+      title,
+      institution,
+      location,
+      startDate,
+      endDate,
+      grade,
+      description,
+    } = req.body;
+
+    const updateFields = {
+      title: title?.trim(),
+      institution: institution?.trim(),
+    };
+    if (location) updateFields.location = location.trim();
+    if (startDate) updateFields.startDate = new Date(startDate);
+    if (endDate) {
+      updateFields.endDate = new Date(endDate);
+      isCurrent: false;
+    } else {
+      isCurrent: true;
+    }
+
+    if (grade) updateFields.grade = grade.trim();
+    if (description) updateFields.description = description.trim();
+
+    let updateEducations = null;
+    console.log(req?.user?.role);
+    if (req?.user?.role == "super_admin") {
+      updateEducations = await educationModel.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        updateFields,
+        { new: true }
+      );
+    } else {
+      updateEducations = await educationModel.findOneAndUpdate(
+        {
+          createdBy: userId,
+          _id: id,
+        },
+        updateFields,
+        { new: true }
+      );
+    }
+
+    if (!updateEducations) {
+      return next(new CustomError("Education not found.", 404));
+    }
+
+    return res.status(200).json({
+      message: "Education updated successfully.",
+      data: updateEducations,
+      isSuccess: true,
+      isError: false,
+    });
+  } catch (error) {
+    console.error("Error while updating education:", error);
+    return next(new CustomError("Internal server error", 500));
+  }
+};
--- backend/routes/education/education.routes.js
+++ backend/routes/education/education.routes.js
@@ -1,9 +1,28 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/auth/authCheck.middleware.js";
-import { handleCreateEducation } from "../../controllers/education/education.controller.js";
+import {
+  handleCreateEducation,
+  handleDeleteEducation,
+  handleGetAllAdminEducation,
+  handleUpdateEducation,
+} from "../../controllers/education/education.controller.js";
+import { verifyObjectId } from "../../middlewares/common/verifyId.js";
 
 const router = express.Router();
 
 router.post("/create-education", authMiddleware, handleCreateEducation);
+router.get("/get-all-education", authMiddleware, handleGetAllAdminEducation);
+router.delete(
+  "/delete-education/:id",
+  verifyObjectId,
+  authMiddleware,
+  handleDeleteEducation
+);
+router.put(
+  "/update-education/:id",
+  verifyObjectId,
+  authMiddleware,
+  handleUpdateEducation
+);
 
 export default router;
